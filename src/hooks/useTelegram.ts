dfac1d67f57fe9b1fbfea934182b4787e6408736
Ruslan Bibomatov <advokat114@mail.ru>
2022-11-02 19:53:09 +0700

    Test API 3

--- src/hooks/useTelegram.ts
+++ src/hooks/useTelegram.ts
@@ -2,9 +2,11 @@ export function useTelegram() {
   const tg = window.Telegram.WebApp;
 
   const onClose = () => {
-    tg.sendData({
-      test: "test",
-    });
+    tg.sendData(
+      JSON.stringify({
+        test: "test",
+      })
+    );
   };
 
   return {
